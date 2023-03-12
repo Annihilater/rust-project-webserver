47a612ef306af6dbf942c64d392d8a61e9d391f5
Annihilater <yanmiexingkong@gmail.com>
2023-03-12 10:24:52 +0800

    feat: add 001

--- 001/my_server/src/main.rs
+++ 001/my_server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    // ...
+    println!("收到一些内容");
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
